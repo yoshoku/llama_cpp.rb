6302c0908c92d84c7fd1058eaaac4b6c3430c0ad
yoshoku <yoshoku@outlook.com>
2023-04-10 00:35:47 +0900

    test: add LLaMACpp constants test

--- test/test_llama_cpp.rb
+++ test/test_llama_cpp.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class TestLLaMACpp < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::LLaMACpp::VERSION
+  def test_version_numbers
+    refute_nil(::LLaMACpp::VERSION)
+    refute_nil(::LLaMACpp::LLAMA_CPP_VERSION)
+    refute_nil(::LLaMACpp::LLAMA_FILE_VERSION)
+    refute_nil(::LLaMACpp::LLAMA_FILE_MAGIC)
+    refute_nil(::LLaMACpp::LLAMA_FILE_MAGIC_UNVERSIONED)
   end
 end
