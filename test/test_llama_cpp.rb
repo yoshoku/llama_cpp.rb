8374de4e0e5c25d4c72766f52c94ddf256fa6300
yoshoku <yoshoku@outlook.com>
2023-04-12 14:24:47 +0900

    test: add LLaMACpp module function test

--- test/test_llama_cpp.rb
+++ test/test_llama_cpp.rb
@@ -10,4 +10,11 @@ class TestLLaMACpp < Minitest::Test
     refute_nil(::LLaMACpp::LLAMA_FILE_MAGIC)
     refute_nil(::LLaMACpp::LLAMA_FILE_MAGIC_UNVERSIONED)
   end
+
+  def test_module_functions
+    assert_equal(1, ::LLaMACpp.token_bos)
+    assert_equal(2, ::LLaMACpp.token_eos)
+    assert_instance_of(String, ::LLaMACpp.print_system_info)
+    assert_match(/AVX = \d{1} | AVX2 = \d{1} | AVX512 = \d{1}/, ::LLaMACpp.print_system_info)
+  end
 end
