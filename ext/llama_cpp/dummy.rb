ccda1b4230d06603a7e40cca8b59e71d5ec3c3d1
yoshoku <yoshoku@outlook.com>
2023-05-20 17:18:55 +0900

    chore: remove unsupported file type

--- ext/llama_cpp/dummy.rb
+++ ext/llama_cpp/dummy.rb
@@ -29,9 +29,6 @@ module LLaMACpp
   LLAMA_FTYPE_MOSTLY_Q4_1_SOME_F16 = 4 # tok_embeddings.weight and output. weight are F16
 
   # LLaMA model file type.
-  LLAMA_FTYPE_MOSTLY_Q4_2 = 5
-
-  # LLaMA model file type.
   LLAMA_FTYPE_MOSTLY_Q8_0 = 7
 
   # LLaMA model file type.
