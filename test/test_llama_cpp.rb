b7f8336abef45983a0b0f10e565ebb43428754fe
yoshoku <yoshoku@outlook.com>
2023-08-26 00:37:23 +0900

    chore: remove deprecated test

--- test/test_llama_cpp.rb
+++ test/test_llama_cpp.rb
@@ -6,14 +6,9 @@ class TestLLaMACpp < Minitest::Test
   def test_version_numbers
     refute_nil(::LLaMACpp::VERSION)
     refute_nil(::LLaMACpp::LLAMA_CPP_VERSION)
-    refute_nil(::LLaMACpp::LLAMA_FILE_VERSION)
-    refute_nil(::LLaMACpp::LLAMA_FILE_MAGIC)
-    refute_nil(::LLaMACpp::LLAMA_FILE_MAGIC_UNVERSIONED)
   end
 
   def test_module_functions
-    assert_equal(1, ::LLaMACpp.token_bos)
-    assert_equal(2, ::LLaMACpp.token_eos)
     assert_instance_of(String, ::LLaMACpp.print_system_info)
     assert_match(/AVX = \d{1} | AVX2 = \d{1} | AVX512 = \d{1}/, ::LLaMACpp.print_system_info)
   end
