d9ddd7e69ee2e8456761a2b0d4485fb190f2a9e9
yoshoku <yoshoku@outlook.com>
2024-03-09 23:31:50 +0900

    chore: change embedding accessor name

--- test/test_context_params.rb
+++ test/test_context_params.rb
@@ -11,18 +11,18 @@ class TestContextParams < Minitest::Test
     assert_equal(512, @params.n_ctx)
     assert_equal(-1, @params.seed)
     refute(@params.logits_all)
-    refute(@params.embedding)
+    refute(@params.embeddings)
   end
 
   def test_setter_and_getter
     @params.n_ctx = 1024
     @params.seed = 1
     @params.logits_all = true
-    @params.embedding = true
+    @params.embeddings = true
 
     assert_equal(1024, @params.n_ctx)
     assert_equal(1, @params.seed)
     assert(@params.logits_all)
-    assert(@params.embedding)
+    assert(@params.embeddings)
   end
 end
