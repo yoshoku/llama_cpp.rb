a8cce1bfffef443f6fd393ab9cab3d73c51a6348
yoshoku <yoshoku@outlook.com>
2024-06-08 11:05:18 +0900

    chore!: remove mpi option

--- ext/llama_cpp/extconf.rb
+++ ext/llama_cpp/extconf.rb
@@ -18,7 +18,6 @@ make_envs << ' LLAMA_BLIS=1' if with_config('blis')
 make_envs << ' LLAMA_CUBLAS=1' if with_config('cublas') # Deprecated, use --with-cuda instead
 make_envs << ' LLAMA_CUDA=1' if with_config('cuda')
 make_envs << ' LLAMA_HIPBLAS=1' if with_config('hipblas')
-make_envs << ' LLAMA_MPI=1' if with_config('mpi')
 make_envs << ' LLAMA_VULKAN=1' if with_config('vulkan')
 make_envs << ' LLAMA_NO_OPENMP=1' if with_config('no-openmp')
 
