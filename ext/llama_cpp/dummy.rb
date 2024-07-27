7499ae80a48fa4580256a3e9046ba8b1eeca292a
yoshoku <yoshoku@outlook.com>
2024-07-27 12:24:55 +0900

    docs: change LLAMA_SESSION_VERSION value

--- ext/llama_cpp/dummy.rb
+++ ext/llama_cpp/dummy.rb
@@ -20,7 +20,7 @@ module LLaMACpp # rubocop:disable Metrics/ModuleLength
   LLAMA_STATE_SEQ_MAGIC = '0x67677371u'
 
   # LLaMA session version.
-  LLAMA_SESSION_VERSION = '6'
+  LLAMA_SESSION_VERSION = '7'
 
   # LLaMA state seq version.
   LLAMA_STATE_SEQ_VERSION = '1'
