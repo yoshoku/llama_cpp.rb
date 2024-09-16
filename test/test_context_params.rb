c2030a808eb5144cc16b0b4486aedaedb7546e0e
yoshoku <yoshoku@outlook.com>
2024-09-16 20:51:59 +0900

    chore: remove seed from ContextParams class test

--- test/test_context_params.rb
+++ test/test_context_params.rb
@@ -9,19 +9,16 @@ class TestContextParams < Minitest::Test
 
   def test_default_values
     assert_equal(512, @params.n_ctx)
-    assert_equal(-1, @params.seed)
     refute(@params.logits_all)
     refute(@params.embeddings)
   end
 
   def test_setter_and_getter
     @params.n_ctx = 1024
-    @params.seed = 1
     @params.logits_all = true
     @params.embeddings = true
 
     assert_equal(1024, @params.n_ctx)
-    assert_equal(1, @params.seed)
     assert(@params.logits_all)
     assert(@params.embeddings)
   end
