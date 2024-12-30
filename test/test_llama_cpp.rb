96a128fe2ceb5d45f60c860400511f4adb633af9
yoshoku <yoshoku@outlook.com>
2024-12-30 13:04:23 +0900

    chore!: remove existing test

--- test/test_llama_cpp.rb
+++ test/test_llama_cpp.rb
@@ -5,11 +5,5 @@ require 'test_helper'
 class TestLLaMACpp < Minitest::Test
   def test_version_numbers
     refute_nil(::LLaMACpp::VERSION)
-    refute_nil(::LLaMACpp::LLAMA_CPP_VERSION)
-  end
-
-  def test_module_functions
-    assert_instance_of(String, ::LLaMACpp.print_system_info)
-    assert_match(/AVX = \d{1} | AVX2 = \d{1} | AVX512 = \d{1}/, ::LLaMACpp.print_system_info)
   end
 end
