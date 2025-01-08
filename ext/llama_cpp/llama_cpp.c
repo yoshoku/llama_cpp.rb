21b7beed69929ab347a4583c57ecbf1a6b8d4bae
yoshoku <yoshoku@outlook.com>
2025-01-08 23:46:15 +0900

    feat: add llama_token_bos module function

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -1566,6 +1566,18 @@ static VALUE rb_llama_token_is_control(VALUE self, VALUE model, VALUE token) {
   return is_control ? Qtrue : Qfalse;
 }
 
+/* llama_token_bos */
+static VALUE rb_llama_token_bos(VALUE self, VALUE model) {
+  if (!rb_obj_is_kind_of(model, rb_cLlamaModel)) {
+    rb_raise(rb_eArgError, "model must be a LlamaModel");
+    return Qnil;
+  }
+  llama_model_wrapper* model_wrapper = get_llama_model_wrapper(model);
+  const int32_t token = llama_token_bos(model_wrapper->model);
+  RB_GC_GUARD(model);
+  return INT2NUM(token);
+}
+
 /* MAIN */
 void Init_llama_cpp(void) {
   char tmp[12];
@@ -2035,4 +2047,7 @@ void Init_llama_cpp(void) {
 
   /* llama_token_is_control */
   rb_define_module_function(rb_mLLaMACpp, "llama_token_is_control", rb_llama_token_is_control, 2);
+
+  /* llama_token_bos */
+  rb_define_module_function(rb_mLLaMACpp, "llama_token_bos", rb_llama_token_bos, 1);
 }
