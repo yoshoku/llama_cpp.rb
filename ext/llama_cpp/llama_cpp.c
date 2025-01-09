3182724b10eaa0dea889185607efd987c2e80d6e
yoshoku <yoshoku@outlook.com>
2025-01-09 22:55:19 +0900

    feat: add llama_token_fim_mid module function

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -1698,6 +1698,18 @@ static VALUE rb_llama_token_fim_suf(VALUE self, VALUE model) {
   return INT2NUM(token);
 }
 
+/* llama_token_fim_mid */
+static VALUE rb_llama_token_fim_mid(VALUE self, VALUE model) {
+  if (!rb_obj_is_kind_of(model, rb_cLlamaModel)) {
+    rb_raise(rb_eArgError, "model must be a LlamaModel");
+    return Qnil;
+  }
+  llama_model_wrapper* model_wrapper = get_llama_model_wrapper(model);
+  const int32_t token = llama_token_fim_mid(model_wrapper->model);
+  RB_GC_GUARD(model);
+  return INT2NUM(token);
+}
+
 /* MAIN */
 void Init_llama_cpp(void) {
   char tmp[12];
@@ -2200,4 +2212,7 @@ void Init_llama_cpp(void) {
 
   /* llama_token_fim_suf */
   rb_define_module_function(rb_mLLaMACpp, "llama_token_fim_suf", rb_llama_token_fim_suf, 1);
+
+  /* llama_token_fim_mid */
+  rb_define_module_function(rb_mLLaMACpp, "llama_token_fim_mid", rb_llama_token_fim_mid, 1);
 }
