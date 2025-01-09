9b51a464a4f98414afe07c31aa6e502a1fb4dc6c
yoshoku <yoshoku@outlook.com>
2025-01-09 21:08:32 +0900

    feat: add llama_token_pad module function

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -1638,6 +1638,18 @@ static VALUE rb_llama_token_nl(VALUE self, VALUE model) {
   return INT2NUM(token);
 }
 
+/* llama_token_pad */
+static VALUE rb_llama_token_pad(VALUE self, VALUE model) {
+  if (!rb_obj_is_kind_of(model, rb_cLlamaModel)) {
+    rb_raise(rb_eArgError, "model must be a LlamaModel");
+    return Qnil;
+  }
+  llama_model_wrapper* model_wrapper = get_llama_model_wrapper(model);
+  const int32_t token = llama_token_pad(model_wrapper->model);
+  RB_GC_GUARD(model);
+  return INT2NUM(token);
+}
+
 /* MAIN */
 void Init_llama_cpp(void) {
   char tmp[12];
@@ -2125,4 +2137,7 @@ void Init_llama_cpp(void) {
 
   /* llama_token_nl */
   rb_define_module_function(rb_mLLaMACpp, "llama_token_nl", rb_llama_token_nl, 1);
+
+  /* llama_token_pad */
+  rb_define_module_function(rb_mLLaMACpp, "llama_token_pad", rb_llama_token_pad, 1);
 }
