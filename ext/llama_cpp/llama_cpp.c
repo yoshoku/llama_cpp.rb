9e157702b8c2e22965ab9c6f3e991c9f0b92dfc8
yoshoku <yoshoku@outlook.com>
2025-01-13 23:42:15 +0900

    chore!: change argument type of llama_vocab_type module function

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -43,13 +43,11 @@ static VALUE llama_vocab_wrapper_alloc(VALUE self) {
   return TypedData_Wrap_Struct(self, &llama_vocab_wrapper_data_type, data);
 }
 
-/*
 static llama_vocab_wrapper* get_llama_vocab_wrapper(VALUE self) {
   llama_vocab_wrapper* data = NULL;
   TypedData_Get_Struct(self, llama_vocab_wrapper, &llama_vocab_wrapper_data_type, data);
   return data;
 }
-*/
 
 /* llama_model wrapper */
 typedef struct {
@@ -726,16 +724,6 @@ static VALUE rb_llama_model_get_vocab(VALUE self, VALUE model) {
   return TypedData_Wrap_Struct(rb_cLlamaVocab, &llama_vocab_wrapper_data_type, vocab_wrapper);
 }
 
-/* llama_vocab_type */
-static VALUE rb_llama_vocab_type(VALUE self, VALUE model) {
-  if (!rb_obj_is_kind_of(model, rb_cLlamaModel)) {
-    rb_raise(rb_eArgError, "model must be a LlamaModel");
-    return Qnil;
-  }
-  llama_model_wrapper* model_wrapper = get_llama_model_wrapper(model);
-  return INT2NUM(llama_vocab_type(model_wrapper->model));
-}
-
 /* llama_model_rope_type */
 static VALUE rb_llama_model_rope_type(VALUE self, VALUE model) {
   if (!rb_obj_is_kind_of(model, rb_cLlamaModel)) {
@@ -794,6 +782,18 @@ static VALUE rb_llama_model_rope_freq_scale_train(VALUE self, VALUE model) {
   }
   llama_model_wrapper* model_wrapper = get_llama_model_wrapper(model);
   return DBL2NUM(llama_model_rope_freq_scale_train(model_wrapper->model));
+}
+
+/* llama_vocab_type */
+static VALUE rb_llama_vocab_type(VALUE self, VALUE vocab) {
+  if (!rb_obj_is_kind_of(vocab, rb_cLlamaVocab)) {
+    rb_raise(rb_eArgError, "vocab must be a LlamaVocab");
+    return Qnil;
+  }
+  llama_vocab_wrapper* vocab_wrapper = get_llama_vocab_wrapper(vocab);
+  VALUE vt = INT2NUM(llama_vocab_type(vocab_wrapper->vocab));
+  RB_GC_GUARD(vocab);
+  return vt;
 }
 
 /* llama_model_desc */
@@ -2390,9 +2390,6 @@ void Init_llama_cpp(void) {
   /* llama_model_get_vocab */
   rb_define_module_function(rb_mLLaMACpp, "llama_model_get_vocab", rb_llama_model_get_vocab, 1);
 
-  /* llama_vocab_type */
-  rb_define_module_function(rb_mLLaMACpp, "llama_vocab_type", rb_llama_vocab_type, 1);
-
   /* llama_model_rope_type */
   rb_define_module_function(rb_mLLaMACpp, "llama_model_rope_type", rb_llama_model_rope_type, 1);
 
@@ -2410,6 +2407,9 @@ void Init_llama_cpp(void) {
 
   /* llama_model_rope_freq_scale_train */
   rb_define_module_function(rb_mLLaMACpp, "llama_model_rope_freq_scale_train", rb_llama_model_rope_freq_scale_train, 1);
+
+  /* llama_vocab_type */
+  rb_define_module_function(rb_mLLaMACpp, "llama_vocab_type", rb_llama_vocab_type, 1);
 
   /* TODO: llama_model_meta_val_str */
   /* TODO: llama_model_meta_count */
