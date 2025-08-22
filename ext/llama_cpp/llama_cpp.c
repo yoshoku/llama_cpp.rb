1864bb395ba3df8104d3d205c6ec01a7ca95fff8
yoshoku <yoshoku@outlook.com>
2025-08-22 23:45:19 +0900

    chore!: remove deprecated llama_kv_self_seq_rm module function

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -2172,37 +2172,6 @@ static VALUE rb_llama_get_memory(VALUE self, VALUE ctx) {
 }
 
 /**
- * @overload llama_kv_self_seq_rm(context, seq_id, p0, p1)
- *  @param [LlamaContext] context
- *  @param [Integer] seq_id
- *  @param [Integer] p0
- *  @param [Integer] p1
- *  @return [Boolean]
- */
-static VALUE rb_llama_kv_self_seq_rm(VALUE self, VALUE ctx, VALUE seq_id, VALUE p0, VALUE p1) {
-  if (!rb_obj_is_kind_of(ctx, rb_cLlamaContext)) {
-    rb_raise(rb_eArgError, "ctx must be a LlamaContext");
-    return Qnil;
-  }
-  if (!RB_INTEGER_TYPE_P(seq_id)) {
-    rb_raise(rb_eArgError, "seq_id must be an Integer");
-    return Qnil;
-  }
-  if (!RB_INTEGER_TYPE_P(p0)) {
-    rb_raise(rb_eArgError, "p0 must be an Integer");
-    return Qnil;
-  }
-  if (!RB_INTEGER_TYPE_P(p1)) {
-    rb_raise(rb_eArgError, "p1 must be an Integer");
-    return Qnil;
-  }
-  llama_context_wrapper* context_wrapper = get_llama_context_wrapper(ctx);
-  const bool res = llama_kv_self_seq_rm(context_wrapper->context, NUM2INT(seq_id), NUM2INT(p0), NUM2INT(p1));
-  RB_GC_GUARD(ctx);
-  return res ? Qtrue : Qfalse;
-}
-
-/**
  * @overload llama_kv_self_seq_cp(context, seq_id_src, seq_id_dst, p0, p1)
  *  @param [LlamaContext] context
  *  @param [Integer] seq_id_src
@@ -5125,9 +5094,6 @@ void Init_llama_cpp(void) {
 
   /* llama_memory_can_shift */
   rb_define_module_function(rb_mLlamaCpp, "llama_memory_can_shift?", rb_llama_memory_can_shift, 1);
-
-  /* llama_kv_self_seq_rm */
-  rb_define_module_function(rb_mLlamaCpp, "llama_kv_self_seq_rm", rb_llama_kv_self_seq_rm, 4);
 
   /* llama_kv_self_seq_cp */
   rb_define_module_function(rb_mLlamaCpp, "llama_kv_self_seq_cp", rb_llama_kv_self_seq_cp, 5);
