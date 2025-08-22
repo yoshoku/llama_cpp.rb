d68e25375e964b97988660f5dca0a3aa867c6c0d
yoshoku <yoshoku@outlook.com>
2025-08-22 23:48:03 +0900

    chore!: remove deprecated llama_kv_self_seq_cp module function

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -2172,42 +2172,6 @@ static VALUE rb_llama_get_memory(VALUE self, VALUE ctx) {
 }
 
 /**
- * @overload llama_kv_self_seq_cp(context, seq_id_src, seq_id_dst, p0, p1)
- *  @param [LlamaContext] context
- *  @param [Integer] seq_id_src
- *  @param [Integer] seq_id_dst
- *  @param [Integer] p0
- *  @param [Integer] p1
- *  @return [NilClass]
- */
-static VALUE rb_llama_kv_self_seq_cp(VALUE self, VALUE ctx, VALUE seq_id_src, VALUE seq_id_dst, VALUE p0, VALUE p1) {
-  if (!rb_obj_is_kind_of(ctx, rb_cLlamaContext)) {
-    rb_raise(rb_eArgError, "ctx must be a LlamaContext");
-    return Qnil;
-  }
-  if (!RB_INTEGER_TYPE_P(seq_id_src)) {
-    rb_raise(rb_eArgError, "seq_id_src must be an Integer");
-    return Qnil;
-  }
-  if (!RB_INTEGER_TYPE_P(seq_id_dst)) {
-    rb_raise(rb_eArgError, "seq_id_dst must be an Integer");
-    return Qnil;
-  }
-  if (!RB_INTEGER_TYPE_P(p0)) {
-    rb_raise(rb_eArgError, "p0 must be an Integer");
-    return Qnil;
-  }
-  if (!RB_INTEGER_TYPE_P(p1)) {
-    rb_raise(rb_eArgError, "p1 must be an Integer");
-    return Qnil;
-  }
-  llama_context_wrapper* context_wrapper = get_llama_context_wrapper(ctx);
-  llama_kv_self_seq_cp(context_wrapper->context, NUM2INT(seq_id_src), NUM2INT(seq_id_dst), NUM2INT(p0), NUM2INT(p1));
-  RB_GC_GUARD(ctx);
-  return Qnil;
-}
-
-/**
  * @overload llama_kv_self_seq_keep(context, seq_id)
  *  @param [LlamaContext] context
  *  @param [Integer] seq_id
@@ -5094,9 +5058,6 @@ void Init_llama_cpp(void) {
 
   /* llama_memory_can_shift */
   rb_define_module_function(rb_mLlamaCpp, "llama_memory_can_shift?", rb_llama_memory_can_shift, 1);
-
-  /* llama_kv_self_seq_cp */
-  rb_define_module_function(rb_mLlamaCpp, "llama_kv_self_seq_cp", rb_llama_kv_self_seq_cp, 5);
 
   /* llama_kv_self_seq_keep */
   rb_define_module_function(rb_mLlamaCpp, "llama_kv_self_seq_keep", rb_llama_kv_self_seq_keep, 2);
