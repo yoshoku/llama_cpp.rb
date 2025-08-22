f06ded37fcac09a6232066213715ed2cc9c61bb9
yoshoku <yoshoku@outlook.com>
2025-08-23 00:11:35 +0900

    chore!: remove deprecated llama_kv_self_update module function

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -2172,22 +2172,6 @@ static VALUE rb_llama_get_memory(VALUE self, VALUE ctx) {
 }
 
 /**
- * @overload llama_kv_self_update(context)
- *  @param [LlamaContext] context
- *  @return [NilClass]
- */
-static VALUE rb_llama_kv_self_update(VALUE self, VALUE ctx) {
-  if (!rb_obj_is_kind_of(ctx, rb_cLlamaContext)) {
-    rb_raise(rb_eArgError, "ctx must be a LlamaContext");
-    return Qnil;
-  }
-  llama_context_wrapper* context_wrapper = get_llama_context_wrapper(ctx);
-  llama_kv_self_update(context_wrapper->context);
-  RB_GC_GUARD(ctx);
-  return Qnil;
-}
-
-/**
  * @overload llama_state_get_size(context)
  *  @param [LlamaContext] context
  *  @return [Integer]
@@ -4891,9 +4875,6 @@ void Init_llama_cpp(void) {
 
   /* llama_memory_can_shift */
   rb_define_module_function(rb_mLlamaCpp, "llama_memory_can_shift?", rb_llama_memory_can_shift, 1);
-
-  /* llama_kv_self_update */
-  rb_define_module_function(rb_mLlamaCpp, "llama_kv_self_update", rb_llama_kv_self_update, 1);
 
   /* llama_state_get_size */
   rb_define_module_function(rb_mLlamaCpp, "llama_state_get_size", rb_llama_state_get_size, 1);
