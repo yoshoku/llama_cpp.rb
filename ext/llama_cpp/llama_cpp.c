ae73b4b7c0483290c062f7c0ef9937b70401a3ab
yoshoku <yoshoku@outlook.com>
2025-10-11 07:47:21 +0900

    feat: add LLAMA_STATE_SEQ_FLAGS_PARTIAL_ONLY constant

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -3951,6 +3951,7 @@ void Init_llama_cpp(void) {
 
   rb_define_const(rb_mLlamaCpp, "LLAMA_TOKEN_NULL", INT2NUM(LLAMA_TOKEN_NULL));
   rb_define_const(rb_mLlamaCpp, "LLAMA_STATE_SEQ_FLAGS_SWA_ONLY", INT2NUM(LLAMA_STATE_SEQ_FLAGS_SWA_ONLY));
+  rb_define_const(rb_mLlamaCpp, "LLAMA_STATE_SEQ_FLAGS_PARTIAL_ONLY", INT2NUM(LLAMA_STATE_SEQ_FLAGS_PARTIAL_ONLY));
 
   sprintf(tmp, "0x%x", LLAMA_FILE_MAGIC_GGLA);
   rb_define_const(rb_mLlamaCpp, "LLAMA_FILE_MAGIC_GGLA", rb_str_new2(tmp));
