5e7739b6193822043382d89113c7119d8ca348a7
yoshoku <yoshoku@outlook.com>
2026-05-09 07:48:23 +0900

    feat: add LLAMA_STATE_SEQ_FLAGS_ON_DEVICE constant value

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -4184,6 +4184,7 @@ void Init_llama_cpp(void) {
   rb_define_const(rb_mLlamaCpp, "LLAMA_TOKEN_NULL", INT2NUM(LLAMA_TOKEN_NULL));
   rb_define_const(rb_mLlamaCpp, "LLAMA_STATE_SEQ_FLAGS_SWA_ONLY", INT2NUM(LLAMA_STATE_SEQ_FLAGS_SWA_ONLY));
   rb_define_const(rb_mLlamaCpp, "LLAMA_STATE_SEQ_FLAGS_PARTIAL_ONLY", INT2NUM(LLAMA_STATE_SEQ_FLAGS_PARTIAL_ONLY));
+  rb_define_const(rb_mLlamaCpp, "LLAMA_STATE_SEQ_FLAGS_ON_DEVICE", INT2NUM(LLAMA_STATE_SEQ_FLAGS_ON_DEVICE));
 
   sprintf(tmp, "0x%x", LLAMA_FILE_MAGIC_GGLA);
   rb_define_const(rb_mLlamaCpp, "LLAMA_FILE_MAGIC_GGLA", rb_str_new2(tmp));
