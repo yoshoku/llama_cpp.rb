5c4779b6fa5520be70eab2f57af3ade5a32b3076
yoshoku <yoshoku@outlook.com>
2026-08-01 15:09:13 +0900

    chore!: remove use_direct_io accessor

--- ext/llama_cpp/llama_cpp.c
+++ ext/llama_cpp/llama_cpp.c
@@ -498,17 +498,6 @@ static VALUE llama_model_params_set_vocab_only(VALUE self, VALUE vocab_only) {
   return vocab_only;
 }
 
-static VALUE llama_model_params_get_use_direct_io(VALUE self) {
-  struct llama_model_params* data = get_llama_model_params(self);
-  return data->use_direct_io ? Qtrue : Qfalse;
-}
-
-static VALUE llama_model_params_set_use_direct_io(VALUE self, VALUE use_direct_io) {
-  struct llama_model_params* data = get_llama_model_params(self);
-  data->use_direct_io = RTEST(use_direct_io) ? true : false;
-  return use_direct_io;
-}
-
 static VALUE llama_model_params_get_use_mlock(VALUE self) {
   struct llama_model_params* data = get_llama_model_params(self);
   return data->use_mlock ? Qtrue : Qfalse;
@@ -4719,17 +4708,6 @@ void Init_llama_cpp(void) {
    * @return [Boolean]
    */
   rb_define_method(rb_cLlamaModelParams, "vocab_only=", RUBY_METHOD_FUNC(llama_model_params_set_vocab_only), 1);
-  /**
-   * Document-method: use_direct_io
-   * @return [Boolean]
-   */
-  rb_define_method(rb_cLlamaModelParams, "use_direct_io", RUBY_METHOD_FUNC(llama_model_params_get_use_direct_io), 0);
-  /**
-   * Document-method: use_direct_io=
-   * @param [Boolean] use_direct_io
-   * @return [Boolean]
-   */
-  rb_define_method(rb_cLlamaModelParams, "use_direct_io=", RUBY_METHOD_FUNC(llama_model_params_set_use_direct_io), 1);
   /**
    * Document-method: use_mlock
    * @return [Boolean]
